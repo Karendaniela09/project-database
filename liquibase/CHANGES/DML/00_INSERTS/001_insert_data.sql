62ac389ca7255d2314c58ae67b0113cd0dcc13df
Karen <klddaniela8@gmail.com>
2026-04-27 17:32:27 -0500

    changelog configurado

---
+++ liquibase/CHANGES/DML/00_INSERTS/001_insert_data.sql
@@ -0,0 +1,6 @@
+-- changeset daniela:4
+INSERT INTO mascota (nombre, especie, edad) VALUES ('Luna', 'Perro', 3);
+INSERT INTO mascota (nombre, especie, edad) VALUES ('Michi', 'Gato', 2);
+
+INSERT INTO veterinario (nombre, especialidad) VALUES ('Dr. Carlos', 'Cirugía');
+INSERT INTO veterinario (nombre, especialidad) VALUES ('Dra. Ana', 'Dermatología');
